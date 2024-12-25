cec456d6c7761e0049e714febb599daea482e7ff
Edson Tomas <edsonhacker13@gmail.com>
2024-12-25 14:42:06 +0100

    Adicionar arquivos de projeto.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite um nome : ");
+            var n = ""; 
+            n = Console.ReadLine();
+            
+            Console.WriteLine("Digite a sua idade");
+
+            int idade = 0;
+
+            try
+            {
+                idade = int.Parse(Console.ReadLine());
+            }catch(Exception e)
+            {
+                Console.WriteLine("Aconteceu alguma coisa " + e.Message);
+                
+            }
+            
+            
+            int dataDeNascimento = 2024 - idade;
+
+            Console.WriteLine("Seu nome: " + n);
+            Console.WriteLine("Sua idade: "+ idade);
+
+            Console.WriteLine("Ano de Nascimento: " + dataDeNascimento);
+        }
+    }
+}
